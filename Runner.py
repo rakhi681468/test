4baf8ad8c15af3b6e5ac734e04e91e6fb68ab295
rakhi681468 <103086988+rakhi681468@users.noreply.github.com>
2022-04-06 08:59:21 +0530

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -1,5 +1,7 @@
-from robot import run
 import os
+import tarfile
+
+from robot import run
 
 
 def execute_tests():
@@ -7,6 +9,7 @@ def execute_tests():
     status = run(testcase_path, listener="allure_robotframework")
     # status = run.run(testcase_path, listener = "allure_robotframework")
     return status
+
 
 def list_files():
     files = os.listdir(os.curdir)
@@ -17,5 +20,15 @@ def list_files():
     print(files)
 
 
+def tardir(path, tar_name):
+    with tarfile.open(tar_name, "w:gz") as tar_handle:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                tar_handle.add(os.path.join(root, file))
+    tar_handle.close()
+
+
 execute_tests()
+tardir('output', 'sample.tar.gz')
 list_files()
+
